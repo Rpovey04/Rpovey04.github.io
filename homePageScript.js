7f1ccb7afaf6769a85a68f0e9df63415e11d15a5
Rhys Povey <rhys.povey82@gmail.com>
2024-09-17 04:29:39 +0100

    js reprogramming

--- homePageScript.js
+++ homePageScript.js
@@ -24,7 +24,7 @@ function startFunction() {	// main
 		leftChars.push(document.createElement("div"));
 		leftChars[i].setAttribute('class', 'floatingCharacter');
 		leftChars[i].innerHTML = displayCharacters[Math.floor(Math.random() * 6)];
-		leftInitialHeights.push(Math.floor(((yoffset + Math.abs((Math.random() * 20) - 10)) % 70) + 15));
+		leftInitialHeights.push(Math.floor(((yoffset + Math.abs((Math.random() * 20) - 10)) % 70) + 5));
 		leftChars[i].style.left = Math.floor((xoffset + Math.abs((Math.random() * 20) - 10)) % 100) + "%";
 		leftChars[i].style.top = (leftInitialHeights[i]) + "%";
 		leftContainer.appendChild(leftChars[i]);
@@ -32,7 +32,7 @@ function startFunction() {	// main
 		rightChars.push(document.createElement("div"));
 		rightChars[i].setAttribute('class', 'floatingCharacter');
 		rightChars[i].innerHTML = displayCharacters[Math.floor(Math.random() * 6)];
-		rightInitialHeights.push(Math.floor(((yoffset + Math.abs((Math.random() * 20) - 10)) % 70) + 15));
+		rightInitialHeights.push(Math.floor(((yoffset + Math.abs((Math.random() * 20) - 10)) % 70) + 5));
 		rightChars[i].style.left = Math.floor((xoffset + Math.abs((Math.random() * 20) - 10)) % 100) + "%";
 		rightChars[i].style.top = (rightInitialHeights[i]) + "%";
 		rightContainer.appendChild(rightChars[i]);
@@ -58,8 +58,8 @@ function animateCharacters() {		// floating characters at load
 		for(let i = 0; i < numChars; i++) {
 			// rounds numbers to avoid floating point inaccuracy
 			leftChars[i].style.top = Math.ceil(Number(leftChars[i].style.top.slice(0, -1))) + "%";
+			leftChars[i].innerHTML = Math.ceil(Number(leftChars[i].style.left.slice(0, -1))) + ", " + Math.ceil(Number(leftChars[i].style.top.slice(0, -1))) ;
 			rightChars[i].style.top = Math.ceil(Number(rightChars[i].style.top.slice(0, -1))) + "%";	
-			// leftChars[i].innerHTML = "(" + leftChars[i].style.left + "," + leftChars[i].style.top + ")";
 		}
 					
 		//starting snake game / ending animation
@@ -68,127 +68,72 @@ function animateCharacters() {		// floating characters at load
 	}
 }
 		
-//---SNAKE---
-		
-let count = 0;
-let head = null;
-let tmpHold = null;
-let detected = 0;
-let addOnNextTurn = 0;
-let dead = 1;		// initially dead
-let direction = [];		// can be [1, 0], [-1, 0], [0, 1], [0, -1]
-let snakeBody = [];		// array of body coordinates
-let snakeBodyHTML = [];
-let choice = null;
-		
+// SNAKE 2
+
+let snakeHTML = [];		// HTML elements
+let snakeBody = [];		// logical positions
+let dead = 1;
+let leftBorder = 100;
+let rightBorder = 0;
+let topBorder = 100;
+let bottomBorder = 0;
+let firstInit = 0;
+
 function initSnake() {
-	for (let i = 0; i < snakeBodyHTML.length; i++) {snakeBodyHTML[i].remove();}
+	if (firstInit == 0){
+		for (let i = 0; i < numChars; i++) {
+			let t = Number(leftChars[i].style.top.slice(0, -1));
+			let l = Number(leftChars[i].style.left.slice(0, -1));
+			if (l < leftBorder) {leftBorder = l;}
+			if (l > rightBorder) {rightBorder = l;}
+			if (t < topBorder) {topBorder = t;}
+			if (t > bottomBorder) {bottomBorder = t;}
+		}
+	}
 	
-	snakeBodyHTML = [];
-	snakeBody = [];
-	direction = [0, 1];		// always starts north
-	let initialX = Math.floor(Math.random() * 80) + 10; 
-	let initialY = Math.floor(Math.random() * 80) + 10;
-			
-	snakeBody.push([initialX, initialY]);
-	snakeBodyHTML.push(document.createElement("div"));
-	snakeBodyHTML[0].setAttribute('class', 'snakeBody');
-	snakeBodyHTML[0].style.left = snakeBody[0][0] + "%";
-	snakeBodyHTML[0].style.top = snakeBody[0][1] + "%";
-	leftContainer.appendChild(snakeBodyHTML[0]);
-			
-	snakeBody.push([initialX, initialY+1]);
-	snakeBodyHTML.push(document.createElement("div"));
-	snakeBodyHTML[1].setAttribute('class', 'snakeBody');
-	snakeBodyHTML[1].style.left = snakeBody[1][0] + "%";
-	snakeBodyHTML[1].style.top = snakeBody[1][1] + "%";
-	leftContainer.appendChild(snakeBodyHTML[1]);
-			
-	snakeBodyHTML[0].innerHTML = "*";
-	snakeBodyHTML[1].innerHTML = "*";
+	for (let i = 0; i < snakeHTML.length; i++) {snakeHTML[i].remove();}
+	
+	snakeHTML = [];
+	
+	snakeHTML.push(document.createElement("div"));		// head
+	snakeHTML[0].setAttribute('class', 'snakeBody');
+	snakeHTML[0].innerHTML = leftBorder + ", " + topBorder ;
+	snakeHTML[0].style.left = leftBorder + "%";
+	snakeHTML[0].style.top = topBorder + "%";
+	leftContainer.appendChild(snakeHTML[0]);
+	
+	snakeHTML.push(document.createElement("div"));		// first body segment
+	snakeHTML[1].setAttribute('class', 'snakeBody');
+	snakeHTML[1].innerHTML = rightBorder + ", " + bottomBorder;
+	snakeHTML[1].style.left = rightBorder + "%";
+	snakeHTML[1].style.top = bottomBorder + "%";
+	leftContainer.appendChild(snakeHTML[1]);
+	
 	dead = 0;
-			
+	firstInit = 1;
 }
 
 function moveSnake() {
-	snakeBody.unshift([snakeBody[0][0] + direction[0], snakeBody[0][1] + direction[1]]);
 	
-	if (addOnNextTurn == 0) {
-		snakeBody.pop();
-		tmpHold = snakeBodyHTML.pop();
-	}
-	else {
-		tmpHold = document.createElement("div");
-		tmpHold.setAttribute('class', 'snakeBody');
-		leftContainer.appendChild(tmpHold);
-		addOnNextTurn = 0;
-	}
-			
-	tmpHold.style.left = snakeBody[0][0] + "%";
-	tmpHold.style.top = snakeBody[0][1] + "%";
-	tmpHold.innerHTML = "+";
-	snakeBodyHTML.unshift(tmpHold);
-			
-	// ASCII effect on body parts
-	if (snakeBody[0][0] == snakeBody[1][0]) {
-		if (snakeBody[0][1] > snakeBody[1][1]) {snakeBodyHTML[1].innerHTML = "&#x2193";}
-		else {snakeBodyHTML[1].innerHTML = "^";}
-	}
-	else {
-		if (snakeBody[0][0] > snakeBody[1][0]) {snakeBodyHTML[1].innerHTML = ">";}
-		else {snakeBodyHTML[1].innerHTML = "<";}
-	}
 }
-		
+
 function checkAte() {
-	detected = 0;
-	for (let i = 0; i < leftChars.length && detected == 0; i++) {
-		if (snakeBody[0][0] == Number(leftChars[i].style.left.slice(0, -1)) && snakeBody[0][1] == Number(leftChars[i].style.top.slice(0, -1))){
-			leftChars[i].style.left = (Math.floor(Math.random() * 80) + 10) + "%";
-			leftChars[i].style.top = (Math.floor(Math.random() * 80) + 10) + "%";
-			leftChars[i].innerHTML = displayCharacters[Math.floor(Math.random() * 6)];
-			addOnNextTurn = 1;
-			detected = 1;
-		}
-	}
+	
 }
 
 function checkDead() {
-	if (snakeBody[0][0] <= 0 || snakeBody[0][0] >= 100 || snakeBody[0][1] >= 100 || snakeBody[0][1] <= 0) {dead = 1;}
 	
-	detected = 0;
-	for (let i = 1; i < snakeBody.length && detected == 0; i++) {
-		if (snakeBody[0][0] ==  snakeBody[i][0] && snakeBody[0][1] == snakeBody[i][1]){
-			dead = 1;
-			detected = 1;
-		}
-	}
 }
 
-function makeMove() {		// random for now, will eventually use forward prop on pretrained weights
-	choice = Math.floor(Math.random() * 3);
-	if (direction[0] == 1 && direction[1] == 0) {
-		direction = [[1, 0], [0, 1], [0, -1]][choice];
-		return;
-	}
-	if (direction[0] == -1 && direction[1] == 0) {
-		direction = [[-1, 0], [0, 1], [0, -1]][choice];
-		return;
-	}
-	if (direction[0] == 0 && direction[1] == 1) {
-		direction = [[-1, 0], [1, 0], [0, 1]][choice];
-		return;
-	}
-	if (direction[0] == 0 && direction[1] == -1){
-		direction = [[-1, 0], [1, 0], [0, -1]][choice];
-		return;
-	}
-}
+function makeMove() {
 	
+}
+
 function playSnake() {			// snake main loop
-	count += 1;
 	if (dead == 1) {initSnake();}
-
+	
+	
+	
 	moveSnake();
 	checkAte();
 	checkDead();
